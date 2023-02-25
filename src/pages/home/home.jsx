aa4bd7967d525744b2d8a84e280bad5050fb8b36
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-02-25 17:44:01 +0100

    updated

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import styles from './home.module.css'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+        <div className={styles.content}>
         <div className={styles.texts}></div>
         <div className={styles.sideImg}></div>
+        </div>
     </div>
   )
 }
