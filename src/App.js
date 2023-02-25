a9f171fff8c7f5916d7edcc8556f8d8137c790ca
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-02-25 18:46:01 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './pages/home/home';
+import Navbar from './pages/navbar/navbar';
 
 function App() {
   return (
     <div className ="App">
+      <Navbar />
       <Home />
     </div >
   );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import DesktopBg from "../../assets/home/background-home-desktop.jpg";
 
 export default function Home() {
   return (
-    <div>
-        <div className='flex border-2 border-[#000]'>
-            <h3>Hello</h3>
-        <div className=''></div>
-        <div className=''></div>
-        </div>
+    <div className="border-[2px] border-[#000] h-[100vh] w-[100vw] overflow-hidden">
+      <img
+        src={DesktopBg}
+        alt=""
+        width={100}
+        height={100}
+        className="h-[100%] w-[100%]"
+      />
+      <div className="flex border-2 border-[#000]">
+        <div className=""></div>
+        <div className=""></div>
+      </div>
     </div>
-  )
+  );
 }
